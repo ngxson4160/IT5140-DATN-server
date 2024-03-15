172d5fcec0c138a6783f916d099c2c366bb8229c
ngxson4160 <ng.xson194160@gmail.com>
2024-03-16 03:16:12 +0700

    add prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug'],
+  });
+  
+  const logger = new Logger('NestApplication');
+
+  const port = process.env.PORT;
+  const url = process.env.URL;
+  const prefix = process.env.PREFIX;
+
+  app.setGlobalPrefix(prefix);
+
+  await app.listen(port, () => {
+    logger.log(`🚀 Server start at ${url}/${prefix}/${port}`);
+  });
+  // await app.listen(3000);
 }
 bootstrap();
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,52 @@
+import {
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    super({
+      log: [
+        {
+          emit: 'stdout',
+          level: 'query',
+        },
+        {
+          emit: 'stdout',
+          level: 'error',
+        },
+        {
+          emit: 'stdout',
+          level: 'warn',
+        },
+      ],
+    });
+  }
+
+  async onModuleInit() {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    this.$on('error', (event) => {
+      this.logger.error(JSON.stringify(event));
+    });
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    this.$on('warn', (event) => {
+      this.logger.warn(event);
+    });
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    this.$on('query', (event) => {
+      this.logger.debug(JSON.stringify(event));
+    });
+
+    await this.$connect();
+  }
+}
