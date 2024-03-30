6d6f542b8552702e2bede376f1acf861ca6a2c2e
ngxson4160 <ng.xson194160@gmail.com>
2024-03-31 02:13:58 +0700

    feat get user profile

--- src/module/user/user.controller.ts
+++ src/module/user/user.controller.ts
@@ -1,10 +1,17 @@
 import { Body, Controller, Get, Param, Put, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserData } from 'src/auth/decorator/user-data.decorator';
+import { IUserData } from 'src/_core/type/user-data.type';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('profile')
+  async getUserProfile(@UserData() userData: IUserData) {
+    return this.userService.getUserProfile(userData.id);
+  }
 
   @Get(':id')
   async getDetailUser(@Param('id') id: string) {
--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -9,8 +9,35 @@ import { EUserStatus } from 'src/_core/constant/enum.constant';
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
+  async getUserProfile(id: number) {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) throw new CommonException(MessageResponse.USER.NOT_FOUND(id));
+
+    return {
+      message: MessageResponse.USER.GET_USER_DETAIL,
+      data: {
+        id: user.id,
+        email: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        avatar: user.avatar,
+        dob: user.dob,
+        gender: user.gender,
+        phoneNumber: user.phoneNumber,
+        cv: user.cv,
+        city: user.city,
+        canApplyJob: user.canApplyJob,
+        desiredSalary: user.desiredSalary,
+        yearExperience: user.yearExperience,
+      },
+    };
+  }
+
   async getDetailUser(id: number) {
-    const user = await this.prisma.user.findFirst({
+    const user = await this.prisma.user.findUnique({
       where: { id },
     });
 
